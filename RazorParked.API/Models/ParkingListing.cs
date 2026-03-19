9788065fde3e631bb8f5561a1371f227e76b11a2
Shietsu Mori <mori@uark.edu>
2026-03-19 13:13:27 -0500

    ParkingList

--- RazorParked.API/Models/ParkingListing.cs
+++ RazorParked.API/Models/ParkingListing.cs
@@ -1,16 +1,20 @@
-﻿namespace RazorParked.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorParked.API.Models
 {
     public class ParkingListing
     {
+        [Key]
         public int ListingID { get; set; }
+
         public int HostUserID { get; set; }
         public string Title { get; set; } = "";
         public string? Description { get; set; }
         public string Location { get; set; } = "";
         public decimal PricePerHour { get; set; }
         public bool IsAvailable { get; set; }
-        public DateTime AvailableFrom { get; set; }
-        public DateTime AvailableTo { get; set; }
+        public DateTime? AvailableFrom { get; set; }
+        public DateTime? AvailableTo { get; set; }
         public double? Latitude { get; set; }
         public double? Longitude { get; set; }
     }
