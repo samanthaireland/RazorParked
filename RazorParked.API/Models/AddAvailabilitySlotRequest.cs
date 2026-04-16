884c8d84947efe61003953d4093abbf6612d0a69
kas068 <kas068@uark.edu>
2026-04-16 13:39:43 -0500

    availability

---
+++ RazorParked.API/Models/AddAvailabilitySlotRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace RazorParked.API.Models
+{
+    public class AddAvailabilitySlotRequest
+    {
+        public int HostUserID { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+    }
+}
